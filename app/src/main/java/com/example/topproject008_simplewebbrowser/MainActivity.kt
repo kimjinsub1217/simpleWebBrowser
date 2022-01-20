b261f3d85fe8cd0c2fd762790abc6e63b909d4de
kimjinsub <hikm9985@naver.com>
2022-01-20 20:05:17 +0900

    URL 로딩기능

--- app/src/main/java/com/example/topproject008_simplewebbrowser/MainActivity.kt
+++ app/src/main/java/com/example/topproject008_simplewebbrowser/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.topproject008_simplewebbrowser
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private val addressBar: EditText by lazy {
+        findViewById(R.id.editTextAddressBar)
+    }
+
+    private val webView: WebView by lazy {
+        findViewById(R.id.webView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+        bindViews()
+    }
+
+//    명시된 URL 불러오는 로직
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initViews() {
+        webView.apply{
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true // 자바 스크립트를 사용할 것이라는 코드
+            loadUrl("http://www.goolge.com")
+        }
+    }
+//    이벤트를 바인딩
+//    onEditorActionListener발생했을 때
+//    actionId가 ACTION_DONE이면 웹페이지에 로딩하는 방식
+    private fun bindViews() {
+        addressBar.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                webView.loadUrl(v.text.toString())
+            }
+            return@setOnEditorActionListener false
+        }
     }
 }
